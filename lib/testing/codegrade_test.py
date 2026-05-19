a231ab8eb892177e2918761bf5bef2e61a342049
Mohamed Noor <mohamednoordagane2195@gmail.com>
2026-05-19 20:46:53 +0300

    done

--- lib/testing/codegrade_test.py
+++ lib/testing/codegrade_test.py
@@ -1,4 +1,15 @@
 
-def test_codegrade_placeholder():
-    """Codegrade placeholder test"""
-    assert 1==1
+  import unittest
+  from codegrade import CodeGrade
+
+class TestCodeGrade(unittest.TestCase):
+    def setUp(self):
+        self.codegrade = CodeGrade()
+
+    def test_codegrade_placeholder(self):
+        """Codegrade placeholder test"""
+        self.assertEqual(1, 1)
+if __name__ == '__main__':
+    unittest.main() 
+
+    
--- lib/testing/conftest.py
+++ lib/testing/conftest.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
-def pytest_itemcollected(item):
-    par = item.parent.obj
-    node = item.obj
-    pref = par.__doc__.strip() if par.__doc__ else par.__class__.__name__
-    suf = node.__doc__.strip() if node.__doc__ else node.__name__
-    if pref or suf:
-        item._nodeid = ' '.join((pref, suf))
+import pytest 
+@pytest.fixture(scope="session")
+def setup():
+    print("Setting up the test environment")
+    yield
+    print("Tearing down the test environment")
+
+    
+    
+
